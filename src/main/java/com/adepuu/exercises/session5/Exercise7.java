460fa7760740dfaa70c5a79e23b8739c1b029c96
syafailyas <syafailyasalmuzani@gmail.com>
2024-03-25 14:19:45 +0700

    feat(syafailyas): add Exercise7.java

--- src/main/java/com/adepuu/exercises/session5/Exercise7.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise7.java
@@ -1,5 +1,8 @@
 package com.adepuu.exercises.session5;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Exercise7
 {
     /**
@@ -13,7 +16,41 @@ public class Exercise7
      * Input : s = "rat", t = "car"
      * Output : false
      */
-    public static void main(String[] args)
+    public static class AnagramChecker
     {
+        public static void main(String[] args)
+        {
+            Scanner scanner = new Scanner(System.in);
+
+            // Input the two strings
+            System.out.print("Enter the first string: ");
+            String s = scanner.next();
+            System.out.print("Enter the second string: ");
+            String t = scanner.next();
+
+            // Check if the strings are anagrams
+            boolean result = isAnagram(s, t);
+
+            // Output the result
+            System.out.println("Output: " + result);
+
+            scanner.close();
+        }
+
+        public static boolean isAnagram(String s, String t)
+        {
+            if ( s.length() != t.length() )
+            {
+                return false;
+            }
+
+            char[] sChars = s.toCharArray();
+            char[] tChars = t.toCharArray();
+
+            Arrays.sort(sChars);
+            Arrays.sort(tChars);
+
+            return Arrays.equals(sChars, tChars);
+        }
     }
 }
